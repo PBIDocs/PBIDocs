038debe0cc66eb88de265516b6654e9c90d84d47
PBIDocs <kerndave@gmail.com>
2026-07-30 18:41:04 -0500

    Add static search route handler

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,5 @@
-import { source } from '@/lib/source';
+import { source } from '@/lib/source'; // double-check if your source file is in @/lib/source or @/app/source
 import { createFromSource } from 'fumadocs-core/search/server';
 
-export const { GET } = createFromSource(source, {
-  // https://docs.orama.com/docs/orama-js/supported-languages
-  language: 'english',
-});
+export const revalidate = false; // prevents dynamic server-rendering during export
+export const { staticGET: GET } = createFromSource(source);
